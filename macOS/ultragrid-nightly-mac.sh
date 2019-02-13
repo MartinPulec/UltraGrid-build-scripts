c1e756623d0cc5dece450c53daa9201dbcc915ca
Martin Pulec <pulec@cesnet.cz>
2019-02-13 14:57:32 +0100

    macOS: remove entirely alternative builds

--- macOS/ultragrid-nightly-mac.sh
+++ macOS/ultragrid-nightly-mac.sh
@@ -6,7 +6,6 @@ set -e
 set -x
 
 BUILD_DIR=ultragrid-nightly
-BUILD_DIR_ALT=ultragrid-nightly-alternative
 OAUTH=$(cat $HOME/github-oauth-token)
 
 . $HOME/common.sh
@@ -54,42 +53,3 @@ curl -H "Authorization: token $OAUTH" -H 'Content-Type: application/gzip' -X POS
 
 cd ..
 
-rm -rf $BUILD_DIR_ALT
-
-# alternative
-
-git clone -b master https://github.com/CESNET/UltraGrid.git $BUILD_DIR_ALT
-
-cd $BUILD_DIR_ALT/
-
-#export CFLAGS='-m32'
-#export CXXFLAGS='-m32'
-#export NVCCFLAGS='-m32'
-#export LDFLAGS='-m32'
-export ARCH='-msse2'
-export PKG_CONFIG_PATH=/usr/local/share/ffmpeg-notoolbox/lib/pkgconfig-static:$PKG_CONFIG_PATH
-
-#./autogen.sh --enable-quicktime --disable-jpeg --disable-deltacast --disable-rtsp  --disable-cuda --enable-syphon --disable-aja
-./autogen.sh ${COMMON_FLAGS[@]}
-make osx-gui-dmg
-
-#scp -i /Users/toor/.ssh/id_rsa 'gui/UltraGrid GUI/UltraGrid.dmg' pulec,ultragrid@frs.sourceforge.net:/home/frs/project/ultragrid/UltraGrid-nightly-OSX-32bit-w-QuickTime.dmg
-
-curl -H "Authorization: token $OAUTH" -X GET https://api.github.com/repos/CESNET/UltraGrid/releases/4347706/assets > assets.json
-LEN=`jq "length" assets.json`
-for n in `seq 0 $(($LEN-1))`; do
-	NAME=`jq '.['$n'].name' assets.json`
-	if [ $NAME = "\"UltraGrid-nightly-macos-alt.dmg\"" ]; then
-		ID=`jq '.['$n'].id' assets.json`
-	fi
-done
-
-if [ -n "$ID" ]; then
-	curl -H "Authorization: token $OAUTH" -X DELETE 'https://api.github.com/repos/CESNET/UltraGrid/releases/assets/'$ID
-fi
-
-curl -H "Authorization: token $OAUTH" -H 'Content-Type: application/gzip' -X POST 'https://uploads.github.com/repos/CESNET/UltraGrid/releases/4347706/assets?name=UltraGrid-nightly-macos-alt.dmg&label=macOS%20build%20%28alternative%2C%20without%20SSE4%20and%20videotoolbox%29' -T 'UltraGrid.dmg'
-
-cd ..
-
-#rm -rf $BUILD_DIR_ALT

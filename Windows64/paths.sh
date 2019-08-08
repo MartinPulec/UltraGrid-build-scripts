b8ff52537c2c85fa6965b16b13cfbdf671e37819
Martin Pulec <martin.pulec@seznam.cz>
2019-08-08 15:58:38 +0200

    Win: path for MSVS 2019

--- Windows64/paths.sh
+++ Windows64/paths.sh
@@ -5,7 +5,6 @@ export CPATH=/usr/local/include`[ -n "$CPATH" ] && echo :$CPATH`
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/mingw64/lib/pkgconfig
 export LIBRARY_PATH=/usr/local/lib`[ -n "$LIBRARY_PATH" ] && echo :$LIBRARY_PATH`
 export CPATH=$CPATH:$CUDA_PATH_C/include
-export MSVC11_PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 11.0/
-export MSVC12_PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/
-export PATH=$PATH:$MSVC12_PATH/Common7/IDE/:$MSVC12_PATH/VC/bin/
+export MSVC16_PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Community/VC/Tools/MSVC/14.22.27905/bin/HostX64/x64
+export PATH=$PATH:$MSVC16_PATH
 

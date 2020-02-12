7ce0d17b0e53b402dc83f26e7e438ac455d49f89
Martin Pulec <martin.pulec@seznam.cz>
2020-02-12 13:55:16 +0100

    MacOS: remove hardcoded pkg-config path

--- macOS/paths.sh
+++ macOS/paths.sh
@@ -6,7 +6,7 @@ export LIBRARY_PATH=/opt/local/lib:/usr/local/cuda/lib:$QT/lib:$NDI/lib/x64
 export DYLD_LIBRARY_PATH=/usr/local/cuda/lib:$NDI/lib/x64
 export PATH=/opt/local/bin:$PATH:/usr/local/bin:/usr/local/cuda/bin:$QT/bin
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH${PKG_CONFIG_PATH:+:}/usr/local/lib/pkgconfig:$QT/lib/pkgconfig
-export PKG_CONFIG=/opt/local/bin/pkg-config
+#export PKG_CONFIG=/opt/local/bin/pkg-config
 export AJA_DIRECTORY=/Users/toor/ntv2sdkmac
 export EXTRA_LIB_PATH=$DYLD_LIBRARY_PATH # needed for make, see Makefile.in
 

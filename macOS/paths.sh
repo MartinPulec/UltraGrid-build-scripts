adfc09a6d32405719fbc057289b07e1fb77a701b
Martin Pulec <martin.pulec@cesnet.cz>
2023-12-05 15:04:01 +0100

    mac adjust paths

--- macOS/paths.sh
+++ macOS/paths.sh
@@ -3,12 +3,12 @@ NDI=/Library/NDI
 NDI_LIB=$NDI/lib/macOS
 
 export CPATH=$CPATH${CPATH:+:}/opt/local/include:/usr/local/include:/usr/local/cuda/include:$QT/include:$NDI/include
-export LIBRARY_PATH=/opt/local/lib:/usr/local/cuda/lib:$QT/lib:$NDI_LIB
+export LIBRARY_PATH=/opt/local/lib:/usr/local/lib:/usr/local/cuda/lib:$QT/lib:$NDI_LIB
 export PATH=/opt/local/bin:$PATH:/usr/local/bin:/usr/local/cuda/bin:$QT/bin
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH${PKG_CONFIG_PATH:+:}/usr/local/lib/pkgconfig:$QT/lib/pkgconfig
 #export PKG_CONFIG=/opt/local/bin/pkg-config
 export AJA_DIRECTORY=/Users/toor/ntv2sdkmac
-export DYLD_LIBRARY_PATH=/usr/local/cuda/lib:$NDI_LIB
+export DYLD_LIBRARY_PATH=/usr/local/cuda/lib:/usr/local/lib:$NDI_LIB
 export EXTRA_LIB_PATH=$DYLD_LIBRARY_PATH # needed for make, see Makefile.in, old UltraGrid, TOREMOVE
 export DYLIBBUNDLER_FLAGS="-s /usr/local/cuda/lib -s $NDI_LIB -s /usr/local/lib -s /opt/local/lib" # new UG
 

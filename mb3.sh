661ba6f7e19543e5d3d750f75ef95eee767ef639
Martin Pulec <martin.pulec@cesnet.cz>
2024-02-06 11:44:29 +0100

    arm macos: Syphon install notice

--- mb3.sh
+++ mb3.sh
@@ -1,3 +1,5 @@
+# Syphon insatll:
+# xcodebuild 'LD_DYLIB_INSTALL_NAME=@executable_path/../Frameworks/Syphon.framework/Versions/A/Syphon'
 ./autogen.sh --enable-qt
 make -j 12 gui-bundle
 .github/scripts/macOS/sign.sh uv-qt.app

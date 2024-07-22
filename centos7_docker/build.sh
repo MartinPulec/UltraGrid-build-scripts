031eb9ad716efd3737d6c5b093afb0b66705fa8c
Martin Pulec <martin.pulec@cesnet.cz>
2024-07-22 09:45:49 +0200

    centos7: enable rtsp (client+server)

--- centos7_docker/build.sh
+++ centos7_docker/build.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/sh -x
 
 source scl_source enable devtoolset-11
+
+set -e
 
 cd /root/ultragrid
 git fetch
 git reset --hard origin/master
-./autogen.sh  --enable-qt --enable-plugins
+./autogen.sh --enable-qt --enable-plugins\
+ --enable-rtsp-server --enable-rtsp
 make -j 12
 rm UltraGrid-*-x86_64.AppImage
 ./data/scripts/Linux-AppImage/create-appimage.sh

ed88ab15b5b8db0705232478fb437ab573cbe2d6
Martin Pulec <martin.pulec@cesnet.cz>
2026-02-20 15:19:35 +0100

    mac: disable aes67 (std::to_chars not present)

--- macOS/ultragrid-nightly-mac-continuous.sh
+++ macOS/ultragrid-nightly-mac-continuous.sh
@@ -76,7 +76,8 @@ do
         GITHUB_REF=
         . .github/scripts/environment.sh
 
-        ./autogen.sh $FEATURES --disable-vulkan --disable-lavc-cuda-conv --enable-sdl=2 --enable-ndi=5
+        ./autogen.sh $FEATURES --disable-vulkan --disable-lavc-cuda-conv --enable-sdl=2 --enable-ndi=5 \
+               --with-object-remove="src/audio/capture/aes67.o|src/audio/playback/aes67.o"
         make -j $(nproc) "QMAKE_FLAGS=QMAKE_CC=$CC QMAKE_CXX=$CXX" gui-bundle
         make osx-gui-dmg
 

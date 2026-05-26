01431463401f554f3e4cdd6004ce5a728c2f19e4
Martin Pulec <martin.pulec@cesnet.cz>
2026-05-26 11:53:31 +0200

    alma8_docker: take ug sources from host
    this would allow provide own ultragrid that is not GH/CESNET/UltraGrid

--- alma8_docker/README.md
+++ alma8_docker/README.md
@@ -6,4 +6,15 @@ docker build -f Dockerfile -t ultragrid-alma8 .
 
 ##  Run (actual build)
 
-run `docker_build.sh` where the above image is prepared
+Run `docker_build.sh` where the above image is prepared. This will
+clone/update ultragrid/ subdirectory used by the actuall build.
+
+If you do not want to build from upstream, just place your code to
+ultragrid/ subdir as `docker_build.sh` does and run (last line) from
+the script:
+```
+docker run -ti --rm -v "$PWD":/root/mnt ultragrid-alma8 /root/mnt/build.sh
+```
+
+(optionally configure for root-owned files not to leak from Docker run
+to ultragrid/ directory - in `ultragrid/` run `./autogen.sh && make clean`)
--- alma8_docker/build.sh
+++ alma8_docker/build.sh
@@ -4,9 +4,21 @@ set -e
 
 export LD_LIBRARY_PATH=/usr/local/lib
 
-git pull
-./autogen.sh --enable-qt --enable-plugins \
+cd "$(mktemp -d)"
+
+srcdir=/root/mnt/ultragrid
+if [ ! -f $srcdir/configure ]; then
+  cd $srcdir
+  ./autogen.sh && make distclean
+  cd -
+fi
+$srcdir/configure --enable-qt --enable-plugins \
   --enable-gpujpeg
-make -j 12
-./data/scripts/Linux-AppImage/create-appimage.sh
+make -j "$(nproc)"
+export GIT_DIR=$srcdir/.git
+git config --global --add safe.directory "$srcdir"
+VERSION=$(git rev-parse --short HEAD)
+export VERSION
+$srcdir/data/scripts/Linux-AppImage/create-appimage.sh
+make -C $srcdir/tools clean && rm -rf $srcdir/tools/src
 mv UltraGrid-*-x86_64.AppImage /root/mnt

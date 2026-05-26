7063d9e474820469240dcbb7ec6554a633ad2e81
Martin Pulec <martin.pulec@cesnet.cz>
2026-05-26 11:53:31 +0200

    alma8_docker: take ug sources from host
    this would allow provide own ultragrid that is not GH/CESNET/UltraGrid

--- alma8_docker/docker_build.sh
+++ alma8_docker/docker_build.sh
@@ -1,2 +1,17 @@
-#!/bin/sh
-docker run -ti --rm -v $PWD:/root/mnt ultragrid-alma8 /root/mnt/build.sh
+#!/bin/sh -e
+
+export GIT_DIR="$PWD"/ultragrid/.git
+if [ -d "$GIT_DIR" ]; then
+  cd ultragrid
+  git fetch http://github.com/CESNET/UltraGrid.git master
+  git checkout FETCH_HEAD
+else
+  git clone --depth 1 http://github.com/CESNET/UltraGrid.git ultragrid
+  cd ultragrid
+fi
+git config safe.directory '*'
+./autogen.sh # generate configure + al.
+make distclean # remove dist files
+cd -
+
+docker run -ti --rm -v "$PWD":/root/mnt ultragrid-alma8 /root/mnt/build.sh
